2302bb7cf7b8a0f054020111b3d96f6cf7e671aa
Clément Gindrier <helazior@protonmail.com>
2022-10-19 10:43:54 +0200

    joli tests

--- include/second_preim_48_fillme.h
+++ include/second_preim_48_fillme.h
@@ -7,6 +7,7 @@
 #include <math.h>
 
 void speck48_96(const uint32_t k[4], const uint32_t p[2], uint32_t c[2]);
+void speck48_96_inv(const uint32_t k[4], const uint32_t c[2], uint32_t p[2]);
 uint64_t cs48_dm(const uint32_t m[4], const uint64_t h);
 
 #endif // !__SECOND_PREIM__
